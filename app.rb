803bc56697035c7d21da45dae455a1bd385a5663
yijordan <yij@uchicago.edu>
2025-02-20 23:57:49 +0000

    dynamic number of rolls and sides complete

--- app.rb
+++ app.rb
@@ -52,3 +52,19 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dynamic/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
